f47a19e17efea6acd2baf8c23cf41938d52b6c3a
Yousef <yousefjavaherian@gmail.com>
2023-03-28 19:00:03 +0330

    feat: show vless link in logs

--- entry.sh
+++ entry.sh
@@ -4,4 +4,5 @@
 /bin/sed -i "s/\$PRIVATE_KEY/$PRIVATE_KEY/g" /etc/xray/config.json
 /bin/sed -i "s/\$SHORT_ID/$SHORT_ID/g" /etc/xray/config.json
 cat /etc/xray/config.json
+echo "vless://$UUID@$SERVER_IP:$PORT?path=%2F&security=reality&encryption=none&pbk=$PUBLIC_KEY&fp=chrome&type=http&sni=yahoo.com&sid=$SHORT_ID#VLESS-REALITY"
 exec "$@"
